72bc7ae4d351d076e186bc82364e4de19629e7dd
Mark Showalter <mshowalter@seti.org>
2012-01-02 18:02:04 +0000

    See notes in revised files.
    Broadcastable items have been re-factored to remove circular loading
    dependencies. Empty.py and Array.py are now separate files rather than
    buried under Scalar.py
    
    The Instrument class has been modified substantially for my photometry work.
    I have a nearly complete set of subclasses defined for the HST instruments,
    but these are still under review. Same goes for class Observation and its
    new subclass Snapshot.
    
    julian.py no longer does ISO parsing based on the pyparsing module. It is
    now MUCH MUCH faster at parsing ISO formats, and also operates on arrays of
    ISO data-time text strings simultaneously. For simplicity elsewhere, I added
    a routine tai_from_iso() that converts to TAI directly, avoiding the need
    for the intermediate step of converting to days and seconds first.
    
    Class Tabulation provides a mechanism to handle functions modeled as tables
    of x and y values. I am using it mainly for calculating the spectral
    bandpasses of HST instruments. It may have other uses as well.
    
    solar.py provides models of the solar spectrum. Used for some calibration
    purposes.

--- tabulation.py
+++ tabulation.py
@@ -26,6 +26,15 @@ class Tabulation(object):
                     x-coordinates.
         """
 
+        ignore = self._update(x,y)
+
+########################################
+# Private methods
+########################################
+
+    def _update(self, x, y):
+        """Updates a tabulation in place with new x and y arrays."""
+
         x = np.asfarray(x)
         y = np.asfarray(y)
         sorted = np.sort(x)
@@ -46,6 +55,84 @@ class Tabulation(object):
             raise ValueError("x-coordinates are not monotonic")
 
         self.func = None
+        return self
+
+    def _update_y(self, new_y):
+        """Updates a tabulation in place with a new y array."""
+
+        y = np.asfarray(y)
+
+        if new_y.shape != self.x.shape:
+            raise ValueError("x and y arrays do not have the same size")
+
+        self.y = y
+        self.func = None
+        return self
+
+    def _trim(self):
+        """Updates the given Tabulation by deleting leading and trailing regions
+        of the domain that contain nothing but zeros. This is never strictly
+        necessary but can improve efficiency and reduce memory requirements. It
+        can be useful because many filter bandpass functions contains strings of
+        zeros at one end or the other.
+
+        NOTE that this function operates in-place, returning the same
+        Tabulation object.
+        """
+
+        # Trim the trailing end
+        (new_x, new_y) = Tabulation._trim1(self.x[::-1], self.y[::-1])
+
+        # Trim the leading end
+        (new_x, new_y) = Tabulation._trim1(new_x[::-1], new_y[::-1])
+
+        return self._update(new_x, new_y)
+
+    @staticmethod
+    def _trim1(x,y):
+        """Private procedure used by trim() to strip away the leading end of
+        an (x,y) array pair.
+        """
+
+        # Define a mask at the low end
+        mask = np.cumsum(y != 0.) != 0
+
+        # Shift left by one to keep last zero
+        mask[:-1] = mask[1:]
+
+        return (x[mask], y[mask])
+
+    @staticmethod
+    def _xmerge(x1,x2):
+        """Returns a new array of x-values containing the union of x-values
+        found in each of the given arrays.
+        """
+
+        # Confirm overlap
+        if x1[0] > x2[-1] or x2[0] > x1[-1]:
+            raise ValueError("domains do not overlap")
+
+        # Merge and sort
+        sorted = np.sort(np.hstack((x1, x2)))
+
+        # Locate and remove duplicates
+        mask = np.hstack((sorted[:-1] != sorted[1:], [True]))
+        return sorted[mask]
+
+    @staticmethod
+    def _xoverlap(x1,x2):
+        """Returns a new array of x-values containing the union of x-values from
+        each of the given arrays that fall within the intersection of the two
+        domains.
+        """
+
+        new_x = Tabulation._xmerge(x1,x2)
+        mask = (new_x >= max(x1[0],x2[0])) & (new_x <= min(x1[-1],x2[-1]))
+        return new_x[mask]
+
+########################################
+# Standard operators
+########################################
 
     def __call__(self, x):
 
@@ -110,60 +197,77 @@ class Tabulation(object):
         # Note that a constant subtracted from a Tabulation will still return
         # zero outside the domain.
 
-    @staticmethod
-    def _xmerge(x1,x2):
-        """Returns a new array of x-values containing the x-values found in
-        each of the given arrays.
-        """
+    def __imul__(self, other):
 
-        # Confirm overlap
-        if x1[0] > x2[-1] or x2[0] > x1[-1]:
-            raise ValueError("domains do not overlap")
+        # In-place multiplication of two Tabulations
+        if type(other) == type(self):
+            new_x = Tabulation._xoverlap(self.x, other.x)
+            return self._update(new_x, self(new_x) * other(new_x))._trim()
 
-        # Merge and sort
-        sorted = np.sort(np.hstack((x1, x2)))
+        # Otherwise just scale the y-values
+        elif np.shape(other) == ():
+            return self._update_y(self.y * other)
 
-        # Locate and remove duplicates
-        mask = np.hstack((sorted[:-1] != sorted[1:], [True]))
-        return sorted[mask]
+    def __idiv__(self, other):
 
-    @staticmethod
-    def _xoverlap(x1,x2):
-        """Returns a new array of x-values containing the the x-values from each
-        of the given arrays that fall within the overlapping domain.
-        """
+        # In-place division of two Tabulations
+        if type(other) == type(self):
+            new_x = Tabulation._xoverlap(self.x, other.x)
+            return self._update(new_x, self(new_x) / other(new_x))._trim()
 
-        new_x = Tabulation._xmerge(x1,x2)
-        mask = (new_x >= max(x1[0],x2[0])) & (new_x <= min(x1[-1],x2[-1]))
-        return new_x[mask]
+        # Otherwise just scale the y-values
+        elif np.shape(other) == ():
+            return self._update_y(self.y / other)
 
-    def _trim(self):
-        """Returns a new tabulation in which leading and trailing regions
-        all equal to zero have been stripped away. This is never actually
-        necessary but can improve efficiency and reduce memory requirements.
-        """
+    def __iadd__(self, other):
 
-        # Trim the trailing end
-        (new_x, new_y) = Tabulation._trim1(self.x[::-1], self.y[::-1])
+        # In-place addition of two Tabulations
+        if type(other) == type(self):
+            new_x = Tabulation._xmerge(self.x, other.x)
+            return self._update(new_x, self(new_x) + other(new_x))
 
-        # Trim the leading end
-        (new_x, new_y) = Tabulation._trim1(new_x[::-1], new_y[::-1])
+        # Otherwise just shift the y-values
+        elif np.shape(other) == ():
+            return self._update_y(self.y + other)
 
-        return Tabulation(new_x, new_y)
+        # Note that a constant added to a Tabulation will still return zero
+        # outside the domain.
 
-    @staticmethod
-    def _trim1(x,y):
-        """Internal procedure used by trim() to strip away the leading end of
-        an (x,y) array pair.
-        """
+    def __isub__(self, other):
 
-        # Define a mask at the low end
-        mask = np.cumsum(y != 0.) != 0
+        # In-place subtraction of two Tabulations
+        if type(other) == type(self):
+            new_x = Tabulation._xmerge(self.x, other.x)
+            return self._update(new_x, self(new_x) - other(new_x))
 
-        # Shift left by one to keep last zero
-        mask[:-1] = mask[1:]
+        # Otherwise just shift the y-values
+        elif np.shape(other) == ():
+            return self._update_y(self.y - other)
 
-        return (x[mask], y[mask])
+        # Note that a constant subtracted from a Tabulation will still return
+        # zero outside the domain.
+
+########################################
+# Additional methods
+########################################
+
+    def trim(self):
+        """Returns a new Tabulation (shallow copy) in which the zero-valued
+        leading and trailing regions of the domain have been removed."""
+
+        # Save the original arrays
+        x = self.x
+        y = self.y
+
+        # Create a trimmed version
+        self._trim()        # operates in-place
+        result = Tabulation(self.x, self.y)
+
+        # Restore the original
+        self.x = x
+        self.y = y
+
+        return result
 
     def domain(self):
         """Returns a tuple containing the range of x-values over which the
@@ -214,20 +318,104 @@ class Tabulation(object):
         dx[:-1] += self.x       # Add the array shifted left
         dx[-1]  += self.x[-1]
 
-        # dx[] is now actually 2x the value at each midpoint
+        # dx[] is now actually 2x the value at each midpoint.
 
         # The weight on each value is the distance between the adjacent midpoints
         dx[:-1] -= dx[1:]   # Subtract the midpoints shifted right (not left)
 
-        # dx[] is now actually -2x the correct value of each weight
+        # dx[] is now actually -2x the correct value of each weight. The last
+        # element is to be ignored.
 
         # The integral is now the sum of the products y * dx
         return -0.5 * np.sum(self.y * dx[:-1])
 
     def resample(self, new_x):
-        """Re-samples a function at a given list of x-values."""
+        """Re-samples a tabulation at a given list of x-values."""
 
         return Tabulation(new_x, self(new_x))
+
+    def subsample(self, new_x):
+        """Resamples a tabulation at the given list of x-values, while at the
+        same time retaining all original x-values."""
+
+        new_x = Tabulation._xmerge(new_x, self.x)
+        return Tabulation(new_x, self(new_x))
+
+    def mean(self, dx=None):
+        """Returns the mean value of the tabulation. If specified, dx is the
+        minimum step permitted along the x-axis during integration."""
+
+        trimmed = self.trim()
+
+        if dx is None:
+            resampled = Tabulation(self.x, self.y.copy())
+                                            # y cannot be a shallow copy...
+        else:
+            (x0,x1) = trimmed.domain()
+            new_x = np.arange(x0 + dx, x1, dx).astype("float")
+            resampled = trimmed.subsample(new_x)
+
+        integ0 = resampled.integral()
+
+        resampled.y *= resampled.x          # ...because we change y in-place
+        integ1 = resampled.integral()
+
+        return integ1/integ0
+
+    def bandwidth_rms(self, dx=None):
+        """Returns the root-mean-square width of the tabulation. This is the
+        mean value of (y * (x - x_mean)**2)**(1/2). If specified, dx is the
+        minimum step permitted along the x-axis during integration."""
+
+        trimmed = self.trim()
+
+        if dx is None:
+            resampled = Tabulation(self.x, self.y.copy())
+                                            # y cannot be a shallow copy...
+        else:
+            (x0,x1) = trimmed.domain()
+            new_x = np.arange(x0 + dx, x1, dx).astype("float")
+            resampled = trimmed.subsample(new_x)
+
+        integ0 = resampled.integral()
+
+        resampled.y *= resampled.x          # ...because we change y in-place
+        integ1 = resampled.integral()
+
+        resampled.y *= resampled.x          # ...twice!
+        integ2 = resampled.integral()
+
+        return np.sqrt(((integ2*integ0 - integ1**2) / integ0**2))
+
+    def pivot_mean(self, precision=0.01):
+        """Returns the "pivot" mean value of the tabulation. The pivot value is
+        the mean value of y(x) d(log(x)). Note all x must be positive."""
+
+        trimmed = self.trim()
+        (x0,x1) = trimmed.domain()
+
+        log_x0 = np.log(x0)
+        log_x1 = np.log(x1)
+        log_dx = np.log(1. + precision)
+
+        new_x = np.exp(np.arange(log_x0, log_x1 + log_dx, log_dx))
+
+        resampled = trimmed.subsample(new_x)
+        integ1 = resampled.integral()
+
+        resampled.y /= resampled.x
+        integ0 = resampled.integral()
+
+        return integ1/integ0
+
+    def fwhm(self, fraction=0.5):
+        max = np.max(self.y)
+        limits = self.locate(max * fraction)
+        assert len(limits) == 2
+        return limits[1] - limits[0]
+
+    def square_width(self):
+        return self.integral() / np.max(self.y)
 
 ########################################
 # UNIT TESTS
@@ -309,6 +497,43 @@ class Test_Tabulation(unittest.TestCase):
         self.assertEqual(product(5.1), 5.1)
         self.assertEqual(product(5.5), 5.5)
         self.assertEqual(product(5.500001), 0.)
+
+        # mean()
+        boxcar = Tabulation((0.,10.),(1.,1.))
+        self.assertEqual(boxcar.mean(), 5.)
+
+        eps = 1.e-14
+        self.assertTrue(np.abs(boxcar.mean(0.33) - 5.) < eps)
+
+        # bandwidth_rms()
+        value = 5. / np.sqrt(3.)
+        eps = 1.e-7
+        self.assertTrue(np.abs(boxcar.bandwidth_rms(0.001) - value) < eps)
+
+        boxcar = Tabulation((10000,10010),(1,1))
+        self.assertEqual(boxcar.mean(), 10005.)
+
+        # pivot_mean()
+        # For narrow functions, the pivot_mean and the mean are similar
+        eps = 1.e-3
+        self.assertTrue(np.abs(boxcar.pivot_mean(1.e-6) - 10005.) < eps)
+
+        # For broad functions, values differ
+        boxcar = Tabulation((1,100),(1,1))
+        value = 99. / np.log(100.)
+        eps = 1.e-3
+        self.assertTrue(np.abs(boxcar.pivot_mean(1.e-6) - value) < eps)
+
+        # fwhm()
+        triangle = Tabulation((0,10,20),(0,1,0))
+        self.assertEqual(triangle.fwhm(), 10.)
+
+        triangle = Tabulation((0,10,20),(0,1,0))
+        self.assertEqual(triangle.fwhm(0.25), 15.)
+
+        # square_width()
+        self.assertEqual(triangle.square_width(), 10.)
+        self.assertEqual(boxcar.square_width(), 99.)
 
 ################################################################################
 # Perform unit testing if executed from the command line
