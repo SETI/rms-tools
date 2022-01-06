5026ddc0faa82cb017e512771a0dcdbe4abeb010
Mark Showalter <mshowalter@seti.org>
2022-01-06 12:29:15 -0800

    Handles whitespace, dashes, underscores, and capitalization better

--- colornames.py
+++ colornames.py
@@ -5,10 +5,11 @@
 # A simple class to import the standard X11 color names into Python. Because I
 # couldn't find a better way to do it.
 #
-# Mark R. Showalter, SETI Institute, June 2009
+# Mark R. Showalter, SETI Institute
 ################################################################################
 
 import re
+
 class ColorNames():
 
 # Extracted from /usr/X11/share/X11/rgb.txt
@@ -766,124 +767,44 @@ class ColorNames():
     "light green"            : (144,238,144),
     "LightGreen"             : (144,238,144) }
 
-    COLOR_NAME_LOWER_DICT = {
-    "ghostwhite"             : (248,248,255),
-    "whitesmoke"             : (245,245,245),
-    "floralwhite"            : (255,250,240),
-    "oldlace"                : (253,245,230),
-    "antiquewhite"           : (250,235,215),
-    "papayawhip"             : (255,239,213),
-    "blanchedalmond"         : (255,235,205),
-    "peachpuff"              : (255,218,185),
-    "navajowhite"            : (255,222,173),
-    "lemonchiffon"           : (255,250,205),
-    "mintcream"              : (245,255,250),
-    "aliceblue"              : (240,248,255),
-    "lavenderblush"          : (255,240,245),
-    "mistyrose"              : (255,228,225),
-    "darkslategray"          : ( 47, 79, 79),
-    "darkslategrey"          : ( 47, 79, 79),
-    "dimgray"                : (105,105,105),
-    "dimgrey"                : (105,105,105),
-    "slategray"              : (112,128,144),
-    "slategrey"              : (112,128,144),
-    "lightslategray"         : (119,136,153),
-    "lightslategrey"         : (119,136,153),
-    "lightgrey"              : (211,211,211),
-    "lightgray"              : (211,211,211),
-    "midnightblue"           : ( 25, 25,112),
-    "navyblue"               : (  0,  0,128),
-    "cornflowerblue"         : (100,149,237),
-    "darkslateblue"          : ( 72, 61,139),
-    "slateblue"              : (106, 90,205),
-    "mediumslateblue"        : (123,104,238),
-    "lightslateblue"         : (132,112,255),
-    "mediumblue"             : (  0,  0,205),
-    "royalblue"              : ( 65,105,225),
-    "dodgerblue"             : ( 30,144,255),
-    "deepskyblue"            : (  0,191,255),
-    "skyblue"                : (135,206,235),
-    "lightskyblue"           : (135,206,250),
-    "steelblue"              : ( 70,130,180),
-    "lightsteelblue"         : (176,196,222),
-    "lightblue"              : (173,216,230),
-    "powderblue"             : (176,224,230),
-    "paleturquoise"          : (175,238,238),
-    "darkturquoise"          : (  0,206,209),
-    "mediumturquoise"        : ( 72,209,204),
-    "lightcyan"              : (224,255,255),
-    "cadetblue"              : ( 95,158,160),
-    "mediumaquamarine"       : (102,205,170),
-    "darkgreen"              : (  0,100,  0),
-    "darkolivegreen"         : ( 85,107, 47),
-    "darkseagreen"           : (143,188,143),
-    "seagreen"               : ( 46,139, 87),
-    "mediumseagreen"         : ( 60,179,113),
-    "lightseagreen"          : ( 32,178,170),
-    "palegreen"              : (152,251,152),
-    "springgreen"            : (  0,255,127),
-    "lawngreen"              : (124,252,  0),
-    "mediumspringgreen"      : (  0,250,154),
-    "greenyellow"            : (173,255, 47),
-    "limegreen"              : ( 50,205, 50),
-    "yellowgreen"            : (154,205, 50),
-    "forestgreen"            : ( 34,139, 34),
-    "olivedrab"              : (107,142, 35),
-    "darkkhaki"              : (189,183,107),
-    "palegoldenrod"          : (238,232,170),
-    "lightgoldenrodyellow"   : (250,250,210),
-    "lightyellow"            : (255,255,224),
-    "lightgoldenrod"         : (238,221,130),
-    "darkgoldenrod"          : (184,134, 11),
-    "rosybrown"              : (188,143,143),
-    "indianred"              : (205, 92, 92),
-    "saddlebrown"            : (139, 69, 19),
-    "sandybrown"             : (244,164, 96),
-    "darksalmon"             : (233,150,122),
-    "lightsalmon"            : (255,160,122),
-    "darkorange"             : (255,140,  0),
-    "lightcoral"             : (240,128,128),
-    "orangered"              : (255, 69,  0),
-    "hotpink"                : (255,105,180),
-    "deeppink"               : (255, 20,147),
-    "lightpink"              : (255,182,193),
-    "palevioletred"          : (219,112,147),
-    "mediumvioletred"        : (199, 21,133),
-    "violetred"              : (208, 32,144),
-    "mediumorchid"           : (186, 85,211),
-    "darkorchid"             : (153, 50,204),
-    "darkviolet"             : (148,  0,211),
-    "blueviolet"             : (138, 43,226),
-    "mediumpurple"           : (147,112,219),
-    "darkgrey"               : (169,169,169),
-    "darkgray"               : (169,169,169),
-    "darkblue"               : (0  ,  0,139),
-    "darkcyan"               : (0  ,139,139),
-    "darkmagenta"            : (139,  0,139),
-    "darkred"                : (139,  0,  0),
-    "lightgreen"             : (144,238,144) }
+    COLOR_NAME_LOWER_DICT = {k.lower().replace(' ',''):v
+                             for k,v in COLOR_NAME_DICT.items()}
 
     RGB_PATTERN = re.compile(
         " *[\[\(] *[0-9]{1,3} *, *[0-9]{1,3} *, *[0-9]{1,3} *[\)\]] *",
         re.IGNORECASE)
 
-
     @staticmethod
     def lookup(name):
 
         # Make sure it's a string
-        if type(name) != type(""):
+        if not isinstance(name, str):
             raise TypeError("Colorname must be a string: " + str(name))
+
+        # Strip quotes and white space if any
+        test = name.strip()
+        if test:
+            if ((test[0] == '"' and test[-1] == '"') or
+                (test[0] == "'" and test[-1] == "'")):
+                    test = test[1:-1].strip()
 
         # Look up the color as a name
         try:
-            return ColorNames.COLOR_NAME_DICT[name.strip()]
+            return ColorNames.COLOR_NAME_DICT[test]
         except KeyError:
             pass
 
         # Try it as lower case
+        test = test.lower()
         try:
-            return ColorNames.COLOR_NAME_LOWER_DICT[name.strip().lower()]
+            return ColorNames.COLOR_NAME_LOWER_DICT[test]
+        except KeyError:
+            pass
+
+        # Try it as lower case, without blanks, dashes or underscores
+        test = test.replace(' ','').replace('-','').replace('_','')
+        try:
+            return ColorNames.COLOR_NAME_LOWER_DICT[test]
         except KeyError:
             pass
 
